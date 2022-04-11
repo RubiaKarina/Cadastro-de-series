40547118b7ed785bab64f89585729dfa7a7c4bfa
Rúbia Karina <rubiakarina22@gmail.com>
2022-04-11 00:08:43 -0300

    commit Cadastro

---
+++ download/DIO.Series/Classes/Serie.cs
@@ -0,0 +1,54 @@
+namespace DIO.Series
+{
+    public class Serie: EntidadeBase
+    {
+        // Atributos
+
+        private Genero Genero { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano { get; set; }
+        private bool Excluido { get; set; }
+
+        // Métodos
+
+        public Serie(int id, Genero Genero, string Titulo, string Descricao, int Ano)
+        {
+            this.id = id;
+            this.Genero = Genero;
+            this.Descricao = Descricao;
+            this.Ano = Ano;
+            this.Excluido = false;
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Gênero: " + this.Genero + Environment.NewLine;
+            retorno += "Título: " + this.Titulo + Environment.NewLine;
+            retorno += "Descrição: " + this.Descricao + Environment.NewLine;
+            retorno += "Ano de Início: " + this.Ano + Environment.NewLine;
+            retorno += "Excluido: " + this.Excluido;
+            return retorno;
+        }
+
+        public string retornaTitulo()
+        {
+            return this.Titulo;
+        }
+
+        public int retornaId()
+        {
+            return this.id;
+        }
+        
+        public bool retornaExcluido()
+        {
+            return this.Excluido;
+        }
+        public void Excluir()
+        {
+            this.Excluido = true;
+        }
+    }
+}
---
+++ download/DIO.Series/Enum/Genero.cs
@@ -0,0 +1,19 @@
+namespace DIO.Series
+{
+
+    public enum Genero
+
+    {
+        Acao = 1,
+        Comedia = 2,
+        Documentario =4,
+        Drama = 5,
+        Espionagem = 6,
+        Faroeste = 7,
+        FiccaoCientifica = 9,
+        Musical = 10,
+        Romance = 11,
+        Supense = 12,
+        Terror = 13
+    }
+}
